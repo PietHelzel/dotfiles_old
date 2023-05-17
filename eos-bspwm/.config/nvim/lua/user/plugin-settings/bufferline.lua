d35e5eb47f43319577fcd4a6e125891cec939a04
Piet Helzel <piet.helzel@outlook.com>
2023-05-17 20:34:43 +0200

    Move nvim out of eos-bspwm folder

--- eos-bspwm/.config/nvim/lua/user/plugin-settings/bufferline.lua
+++
@@ -1,9 +0,0 @@
-require("bufferline").setup{
-    options = {
-        offsets = {{
-            filetype = "NvimTree", text = "", padding = 1 -- Offset from NvimTree
-        }},
-        close_command = "BufDel %d",
-        right_mouse_command = "BufDel %d"
-    },
-}
--- eos-bspwm/.config/nvim/lua/user/plugin-settings/lsp_lines.lua
+++
@@ -1,6 +0,0 @@
-require("lsp_lines").setup()
-
--- Remove normal diagnostic
-vim.diagnostic.config({
-  virtual_text = false,
-})
